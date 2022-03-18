2abcf6b9ddf3b249f22a2ca5a0c4a3407770a730
童锴 <346762712@qq.com>
2022-03-18 17:35:09 +0800

    learn rust

---
+++ learn-rust/restaurant/src/lib.rs
@@ -0,0 +1,47 @@
+fn serve_order() {}
+
+mod front_of_house { // 私有性边界（privacy boundary）
+    pub mod hosting {
+        use crate::serve_order as order;
+
+        pub fn add_to_waitlist() {
+            super::super::serve_order();
+
+            order();
+        }
+    }
+}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+}
